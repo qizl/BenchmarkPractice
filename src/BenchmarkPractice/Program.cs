adee51f402e6c40f6a360210e39f2a6473fbc1c8
qizhenlu <q@qizl.cn>
2019-02-25 12:41:42 +0800

    测试代码

---
+++ src/BenchmarkPractice/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace BenchmarkPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<TestContext>();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/BenchmarkPractice/TestContext.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkPractice.Models;
+
+namespace BenchmarkPractice
+{
+    public class TestContext
+    {
+        [Benchmark]
+        public void TestMD5()
+        {
+            HashHelper.GetMD5("https://www.baidu.com/img/bd_logo1.png");
+        }
+
+        [Benchmark]
+        public void TestSHA1()
+        {
+            HashHelper.GetSHA1("https://www.baidu.com/img/bd_logo1.png");
+        }
+    }
+}
